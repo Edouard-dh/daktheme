db1a29273d04063001cc3cf2ba35ddf02abfbdfb
edouardportable <ed.dhalluin@gmail.com>
2021-05-31 16:15:07 +0200

    Réalisation du projet drak theme avec hook et provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { useDarkMode } from "./useDarkMode";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyles } from "./global";
+import Toggle from "./Toggle";
 
+// The function that toggles between themes
 function App() {
+  const [theme, toggleTheme, componentMounted] = useDarkMode();
+  const themeMode = theme === "light" ? lightTheme : darkTheme;
+
+  if (!componentMounted) {
+    return <div />;
+  }
+
+  // Return the layout based on the current theme
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeMode}>
+      <>
+        <GlobalStyles />
+        <Toggle theme={theme} toggleTheme={toggleTheme} />
+        <h1>It's a {theme === "light" ? "light theme" : "dark theme"}!</h1>
+
+        <footer>
+          <small>
+            <b>Sun</b> icon made by{" "}
+            <a href="https://www.flaticon.com/authors/smalllikeart">
+              smalllikeart
+            </a>{" "}
+            from <a href="https://www.flaticon.com">www.flaticon.com</a>
+          </small>
+          <small>
+            <b>Moon</b> icon made by{" "}
+            <a href="https://www.freepik.com/home">Freepik</a> from{" "}
+            <a href="https://www.flaticon.com">www.flaticon.com</a>
+          </small>
+        </footer>
+      </>
+    </ThemeProvider>
   );
 }
 
